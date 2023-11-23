9fdfefc41ab85fcf22b8be8a5075c35b00d74ac5
btrhub <94720167+btrhub@users.noreply.github.com>
2023-11-23 23:27:15 +0100

    Update my.js

--- my.js
+++ my.js
@@ -7,4 +7,21 @@ function testGS(){
     console.log("click")    
 }
 
+function addGS(){
+    const url = "https://script.google.com/macros/s/AKfycbwazXlrhZjaulEvqyxDiSHnhyTJD0TiB3zOGOmBwQKQMcy-pzmWa8d9S_jFaJVI9Nhg/exec";
+    fetch(url, {
+        method:'POST',
+        mode:'no-cors',
+        cache:'no-cache',
+        credentials:'omit',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        redirect:'follow',
+        body:JSON.stringify({name:'Olivia'})
+    } )
+
+    console.log("click")    
+} 
 document.getElementById("btn").addEventListener("click",testGS)
+document.getElementById("btn2").addEventListener("click",addGS)
